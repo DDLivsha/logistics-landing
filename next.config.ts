251e82fef7a71de9563ea0379e6f67731c2c1f5d
Livsha <ddlivsha@gmail.com>
2025-07-14 15:19:46 +0300

    start

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  webpack(config: any) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
